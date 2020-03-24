97af51b55858d282d2aa91f9d1a8524b1a67f654
sanskarseth <sethsanskar@gmail.com>
2020-03-24 19:43:17 +0530

    2 player game on web page

---
+++ jscript.js
@@ -0,0 +1,52 @@
+let sign = "X";
+let disp = document.getElementById("player");
+let thisone;
+
+
+function printx(number){
+    let thisone = document.getElementById("r"+number);
+  console.log(thisone);
+  
+  if(thisone.innerText==""){
+  thisone.innerText=sign;
+  winner();
+  checksign();
+ disp.innerHTML="<center>"+ sign + "'s Turn.."+"</center>";
+ 
+  }
+  
+  
+}
+function checksign(){
+    if(sign=="X")sign ="O";
+    else sign = "X";
+   
+}
+function getbox(no){
+    return document.getElementById("r"+no).innerHTML;
+}
+function checkmove(a,b,c,m){
+    if(getbox(a)==m && getbox(b)==m && getbox(c)==m)
+       return true;
+    else return false;   
+}
+
+function winner(){
+if(checkmove(1,2,3,sign)||checkmove(4,5,6,sign)||checkmove(7,8,9,sign)
+    ||checkmove(1,4,7,sign)||checkmove(2,5,8,sign)||checkmove(3,6,9,sign)
+    ||checkmove(1,5,9,sign)||checkmove(7,5,3,sign)){
+    disp.innerHTML = "<center>" +sign + " Has Won The Game.." + "</center>";
+    for(let i=1;i<=9;i++){
+        document.getElementById("r"+i).innerHTML="";
+    }
+   throw "game end"; //to forcefully end the game
+}else{
+    if(getbox(1)!=""&& getbox(2)!=""&& getbox(3)!=""&&
+   getbox(4)!=""&& getbox(5)!=""&& getbox(6)!=""&&
+   getbox(7)!=""&& getbox(8)!=""&& getbox(9)!=""){
+
+    disp.innerHTML = "<center> its a tie </center>";
+       throw "It is a Tie..";
+   }
+}
+}
